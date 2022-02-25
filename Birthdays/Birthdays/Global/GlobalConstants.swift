3af55a53711b09aeab31c4ddfe47b5f64c1bc8fc
Piotr Shalatonin <Piotr_Shalatonin@epam.com>
2022-02-25 08:29:12 +0300

    [0h 11m] Add project resources and main folders

---
+++ Birthdays/Birthdays/Global/GlobalConstants.swift
@@ -0,0 +1,8 @@
+//
+//  GlobalConstants.swift
+//  Birthdays
+//
+//  Created by Piotr Shalatonin on 25.02.22.
+//
+
+import Foundation
---
+++ Birthdays/Birthdays/Global/GlobalUtils.swift
@@ -0,0 +1,8 @@
+//
+//  GlobalUtils.swift
+//  Birthdays
+//
+//  Created by Piotr Shalatonin on 25.02.22.
+//
+
+import Foundation
