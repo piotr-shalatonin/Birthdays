a8f379d8112d191c5762c12f590fb9d97695a0b7
Piotr Shalatonin <Piotr_Shalatonin@epam.com>
2022-02-25 08:21:10 +0300

    [0h 2m] Create empty SwiftUI project

---
+++ Birthdays/Birthdays/BirthdaysApp.swift
@@ -0,0 +1,17 @@
+//
+//  BirthdaysApp.swift
+//  Birthdays
+//
+//  Created by Piotr Shalatonin on 25.02.22.
+//
+
+import SwiftUI
+
+@main
+struct BirthdaysApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
